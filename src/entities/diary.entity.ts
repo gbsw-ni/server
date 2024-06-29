8d0de88808e6731d0b0a3bee4f42b402877c4a1e
YoungjinKim <talkingjuly@gmail.com>
2024-06-29 23:31:49 +0900

    entities gitignore에서 제외

---
+++ src/entities/diary.entity.ts
@@ -0,0 +1,9 @@
+interface Diary {
+  id: number
+  userId: number
+  contents: string
+  createdAt: string
+  updatedAt: string
+}
+
+export { Diary }
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,11 @@
+interface User {
+  id: number
+  friendCode: string
+  email: string
+  name: string
+  password: string
+  rating: number
+  ratingLimit: number
+}
+
+export { User }
